0ade6884a00e52315343c2adde8b45799f3ee555
RipSNake <seba.guzman13@gmail.com>
2021-03-02 23:29:13 -0300

    trying to make a request for creating a temporary token to access mapbox client side

--- scripts/mapbox-script.js
+++ scripts/mapbox-script.js
@@ -23,3 +23,54 @@ var geocoder = new MapboxGeocoder({
 });
 
 document.getElementById('search-form').appendChild(geocoder.onAdd(map));
+
+// Use AJAX to request a temporary token
+function createTemporaryToken() {
+	const Http = new XMLHttpRequest();
+	const url = 'https://api.mapbox.com/tokens/v2/ripsnake?access_token='+mapboxgl.accessToken;
+	
+	// To parse object attributes to params
+	var params = new Object();
+	params.expires = Date();
+	params.scopes = ['styles:read', 'fonts:read'];
+
+	// Turn the data object into an array of URL-encoded key/value pairs.
+	let urlEncodedData = "", urlEncodedDataPairs = [], name;
+	for( name in params ) {
+	 urlEncodedDataPairs.push(encodeURIComponent(name)+'='+encodeURIComponent(params[name]));
+	}
+
+	console.log(urlEncodedData);
+	console.log(urlEncodedDataPairs);
+
+	Http.open('POST',url, true);
+
+	Http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	
+	Http.send(urlEncodedDataPairs);
+
+	Http.onreadystatechange = (e) =>{
+		console.log(Http.responseText);
+	}	
+}
+
+function fetchToken() {
+	const url = 'https://api.mapbox.com/tokens/v2/ripsnake?access_token='+mapboxgl.accessToken;
+	reqBody = {
+		expires: (Date() + 300000000000),
+		scopes: ['styles:read', 'fonts:read']
+	};
+	
+	const optParams = {
+		headers: {
+			'content-type':'application/json; charset=UTF-8' 
+		},
+		body:reqBody,
+		method:'POST'
+	}
+
+	fetch(url, optParams)
+		.then(reqBody=>{return data.json})
+		.then(res=>{console.log(res)})
+		.catch(error=>{console.log(error)});
+}
